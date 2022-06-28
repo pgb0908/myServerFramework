700b7e26825f9e08890c5324aaf6649db031472a
gyubong_park <gyubong_park@tmax.co.kr>
2022-06-28 19:44:50 +0900

    first commint

---
+++ example/client/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by bong on 22. 6. 28.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "client" << endl;
+
+    return 0;
+}
---
+++ example/server/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by bong on 22. 6. 28.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "server" << endl;
+
+    return 0;
+}
